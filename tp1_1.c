e096bafc69bc5e1079bbc10f50161737002b7edc
Alejandro <alextimo2003@gmail.com>
2025-04-02 19:05:19 -0300

    segundo commit del archivo

--- tp1_1.c
+++ tp1_1.c
@@ -3,5 +3,15 @@
 int main () {
     printf("Hola mundo");
 
+    int var = 42;  // Declaramos una variable de algún tipo
+    int *ptr = &var; // Declaramos un puntero que apunta a la variable
+
+    // Mostramos la información solicitada
+    printf("1) El contenido del puntero: %d\n", *ptr);
+    printf("2) La dirección de memoria almacenada por el puntero: %p\n", ptr);
+    printf("3) La dirección de memoria de la variable: %p\n", &var);
+    printf("4) La dirección de memoria del puntero: %p\n", &ptr);
+    printf("5) El tamaño de memoria utilizado por la variable: %lu bytes\n", sizeof(var));
+
     return 0;
 }
